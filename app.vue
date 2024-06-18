fb7e42c7f51a8e541ce9ecf5a9ce113b28224611
Khun Aung Paing <khunaungpaing@Khuns-MacBook-Air.local>
2024-06-19 01:04:55 +0630

    project setup

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <nuxt-link to="/blog/login">Login</nuxt-link>
+      <nuxt-link to="/blog/register">Register</nuxt-link>
+      <NuxtPage/>
+    </NuxtLayout>
+  </div>
+</template>
---
+++ pages/blog/login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <div>
+        <label for="email">Email:</label>
+        <UInput id="email" v-model="email" type="email" />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <UInput id="password" v-model="password" type="password" />
+      </div>
+      <div>
+        <UButton type="submit">Login</UButton>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+
+const handleSubmit = () => {
+  // Handle form submission logic here
+  console.log(`Email: ${email.value}, Password: ${password.value}`)
+}
+</script>
